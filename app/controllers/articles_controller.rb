5a9b9fcd68d258a459204cfba21345219ac85726
Brad <116439382+BigBradCute@users.noreply.github.com>
2022-12-13 21:42:26 +0200

    Admin and permissions, all good

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,9 +2,12 @@ class ArticlesController < ApplicationController
   
   
   before_action :set_article, only: [:show, :edit, :update, :destroy]
+  before_action :require_user, except: [:show, :index]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
   def show
     
   end
+
   def index
    @articles = Article.paginate(page: params[:page], per_page: 6)
   end
@@ -29,7 +32,7 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
-    @article.user = User.first
+    @article.user = current_user
     if @article.save
       flash[:notice] = "Article was created successfully."
       redirect_to @article
@@ -39,8 +42,8 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    
     @article.destroy
+    flash[:alert] = "Article was deleted"
     redirect_to articles_path
   end
 
@@ -52,6 +55,13 @@ class ArticlesController < ApplicationController
 
   def article_params
     params.require(:article).permit(:title, :description)
+  end
+
+  def require_same_user
+    if current_user != @article.user && !current_user.admin?
+      flash[:alert] = "You're not allowed to do that"
+      redirect_to @article
+    end
   end
 end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
 
-    before_action :set_user, only: [:edit, :update, :show]
+    before_action :set_user, only: [:edit, :update, :show, :destroy]
+    before_action :require_user, except: [:show, :new, :create]
+    before_action :require_same_user, only: [:dstroy, :edit, :update]
 
     def new
         @user = User.new
@@ -25,6 +27,20 @@ class UsersController < ApplicationController
         end
     end
 
+    def destroy
+        if current_user.admin? 
+            flash[:notice] = "#{@user.username}'s account and articles have been deleted"
+            @user.destroy
+            redirect_to root_path
+        else
+            @user.destroy
+            session[:user_id] = nil
+            flash[:notice] = "Your account and articles have been deleted"
+            redirect_to root_path
+        end
+        
+    end
+
     def edit
 
     end
@@ -45,5 +61,12 @@ class UsersController < ApplicationController
 
         def set_user 
             @user = User.find(params[:id])
+        end
+
+        def require_same_user
+            if @user != current_user && !current_user.admin?
+                flash[:alert] = "You're not allowed to do that"
+                redirect_to @user
+            end
         end
 end
