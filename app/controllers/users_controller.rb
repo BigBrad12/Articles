8601b7d864ed503d85dd7917405e1224fbdcaefc
Brad <116439382+BigBradCute@users.noreply.github.com>
2022-12-13 15:12:52 +0200

    Cleaned navbar, helper methods added for session data usage

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,13 +11,13 @@ class UsersController < ApplicationController
     end
 
     def show
-        @user = User.find(params[:id])
         @articles = @user.articles.paginate(page: params[:page], per_page: 6)
     end
     
     def create
         @user = User.new(user_params)
         if @user.save
+            session[:user_id] = @user.id
             flash[:notice] = "Welcome, #{@user.username}, sign up successful"
             redirect_to @user
         else
@@ -39,11 +39,11 @@ class UsersController < ApplicationController
     end
 
     private
-    def user_params
-        params.require(:user).permit(:username, :email, :password)
-    end
+        def user_params
+            params.require(:user).permit(:username, :email, :password)
+        end
 
-    def set_user 
-        @user = User.find(params[:id])
-    end
+        def set_user 
+            @user = User.find(params[:id])
+        end
 end
